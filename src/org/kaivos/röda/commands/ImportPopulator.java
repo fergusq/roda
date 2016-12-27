8bd50c9f02627298dcacf0f5766db761da5a6632
Iikka Hauhio <iikka@kaivos.org>
2016-12-28 00:33:34 +0200

    korjattu import-funktioiden nimet

--- src/org/kaivos/röda/commands/ImportPopulator.java
+++ src/org/kaivos/röda/commands/ImportPopulator.java
@@ -24,14 +24,14 @@ public final class ImportPopulator {
 				I.loadFile(file, I.G);
 			}
 		}, Arrays.asList(new Parameter("files", false, STRING)), true));
-		S.setLocal("localImport", RödaNativeFunction.of("import", (typeargs, args, kwargs, scope, in, out) -> {
+		S.setLocal("localImport", RödaNativeFunction.of("localImport", (typeargs, args, kwargs, scope, in, out) -> {
 			for (RödaValue value : args) {
 				String filename = value.str();
 				File file = IOUtils.getMaybeRelativeFile(I.currentDir, filename);
 				I.loadFile(file, scope, true);
 			}
 		}, Arrays.asList(new Parameter("files", false, STRING)), true));
-		S.setLocal("safeLocalImport", RödaNativeFunction.of("import", (typeargs, args, kwargs, scope, in, out) -> {
+		S.setLocal("safeLocalImport", RödaNativeFunction.of("safeLocalImport", (typeargs, args, kwargs, scope, in, out) -> {
 			for (RödaValue value : args) {
 				String filename = value.str();
 				File file = IOUtils.getMaybeRelativeFile(I.currentDir, filename);
