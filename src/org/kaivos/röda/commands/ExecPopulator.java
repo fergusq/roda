276e766b7393ea8070929e85f9143f97ab9c1739
Iikka Hauhio <iikka@kaivos.org>
2017-03-21 20:50:52 +0200

    lisätty execiin ja bufferedExeciin mahdollisuus uudelleenohjata virrat

--- src/org/kaivos/röda/commands/ExecPopulator.java
+++ src/org/kaivos/röda/commands/ExecPopulator.java
@@ -25,6 +25,7 @@ import org.kaivos.röda.Interpreter.RödaException;
 import org.kaivos.röda.Interpreter.RödaScope;
 import org.kaivos.röda.Parser;
 import org.kaivos.röda.Parser.DatatypeTree;
+import org.kaivos.röda.RödaStream;
 import org.kaivos.röda.runtime.Function.Parameter;
 import org.kaivos.röda.RödaValue;
 import org.kaivos.röda.type.RödaNativeFunction;
@@ -33,6 +34,32 @@ import org.kaivos.röda.type.RödaString;
 public final class ExecPopulator {
 
 	private ExecPopulator() {}
+	
+	private static void outputThread(InputStream pout, RödaStream out, boolean lineMode) {
+		InputStreamReader reader = new InputStreamReader(pout);
+		try {
+			if (lineMode) {
+				BufferedReader br = new BufferedReader(reader);
+				while (true) {
+					String str = br.readLine();
+					if (str == null)
+						break;
+					out.push(RödaString.of(str));
+				}
+				br.close();
+			} else {
+				while (true) {
+					int chr = reader.read();
+					if (chr == -1)
+						break;
+					out.push(RödaString.of(String.valueOf((char) chr)));
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			error(e);
+		}
+	}
 	
 	public static void addExecFunction(Interpreter I, String name, boolean lineMode) {
 		I.G.setLocal(name, RödaNativeFunction.of(name, (typeargs, args, kwargs, scope, in, out) -> {
@@ -47,13 +74,33 @@ public final class ExecPopulator {
 				envVars.put(e.getKey(), e.getValue().str());
 			}
 			
+			boolean inheritIn = false, inheritOut = false, inheritErr = true;
+			
+			if (kwargs.containsKey("redirect_in")) {
+				RödaValue val = kwargs.get("redirect_in");
+				if (val.is(RödaValue.BOOLEAN)) inheritIn = val.bool();
+			}
+			
+			if (kwargs.containsKey("redirect_out")) {
+				RödaValue val = kwargs.get("redirect_out");
+				if (val.is(RödaValue.BOOLEAN)) inheritOut = val.bool();
+			}
+			
+			if (kwargs.containsKey("redirect_err")) {
+				RödaValue val = kwargs.get("redirect_err");
+				if (val.is(RödaValue.BOOLEAN)) inheritErr = val.bool();
+			}
+			
 			try {
 				ProcessBuilder b = new ProcessBuilder(params);
-				b.redirectError(Redirect.INHERIT);
+				if (inheritIn) b.redirectInput(Redirect.INHERIT);
+				if (inheritOut) b.redirectOutput(Redirect.INHERIT);
+				if (inheritErr) b.redirectError(Redirect.INHERIT);
 				b.directory(I.currentDir);
 				b.environment().putAll(envVars);
 				Process p = b.start();
 				InputStream pout = p.getInputStream();
+				InputStream perr = p.getErrorStream();
 				PrintWriter pin = new PrintWriter(p.getOutputStream());
 				Runnable input = () -> {
 					if (true) {
@@ -67,35 +114,15 @@ public final class ExecPopulator {
 					}
 					pin.close();
 				};
-				Runnable output = () -> {
-					InputStreamReader reader = new InputStreamReader(pout);
-					try {
-						if (lineMode) {
-							BufferedReader br = new BufferedReader(reader);
-							while (true) {
-								String str = br.readLine();
-								if (str == null)
-									break;
-								out.push(RödaString.of(str));
-							}
-							br.close();
-						} else {
-							while (true) {
-								int chr = reader.read();
-								if (chr == -1)
-									break;
-								out.push(RödaString.of(String.valueOf((char) chr)));
-							}
-						}
-						reader.close();
-					} catch (IOException e) {
-						error(e);
-					}
-				};
-				Future<?> futureIn = Interpreter.executor.submit(input);
-				Future<?> futureOut = Interpreter.executor.submit(output);
-				futureOut.get();
-				futureIn.get();
+				Runnable output = () -> outputThread(pout, out, lineMode);
+				Runnable errput = () -> outputThread(perr, out, lineMode);
+				Future<?> futureIn = null, futureOut = null, futureErr = null;
+				if (!inheritIn) futureIn = Interpreter.executor.submit(input);
+				if (!inheritOut) futureOut = Interpreter.executor.submit(output);
+				if (!inheritErr) futureErr = Interpreter.executor.submit(errput);
+				if (!inheritOut) futureOut.get();
+				if (!inheritErr) futureErr.get();
+				if (!inheritIn) futureIn.get();
 				p.waitFor();
 			} catch (IOException e) {
 				error(e);
@@ -109,7 +136,8 @@ public final class ExecPopulator {
 			}
 		}, Arrays.asList(new Parameter("command", false), new Parameter("args", false)), true,
 				Arrays.asList(new Parameter("env", false,
-						Parser.expressionNew("<exec populator>", -1, new DatatypeTree("map"), Collections.emptyList())))));
+						Parser.expressionNew("<exec populator>", -1, new DatatypeTree("map"), Collections.emptyList()))),
+				true));
 	}
 
 	public static void populateExec(Interpreter I, RödaScope S) {
