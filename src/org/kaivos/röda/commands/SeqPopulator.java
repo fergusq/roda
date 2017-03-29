509a76310b1c36169e7bdf57d8a737c9c79fe045
Iikka Hauhio <iikka@kaivos.org>
2017-03-29 20:45:46 +0300

    lisätty makeSeq-funktio

--- src/org/kaivos/röda/commands/SeqPopulator.java
+++ src/org/kaivos/röda/commands/SeqPopulator.java
@@ -2,6 +2,7 @@ package org.kaivos.röda.commands;
 
 import static org.kaivos.röda.Interpreter.outOfBounds;
 import static org.kaivos.röda.Parser.expressionInt;
+import static org.kaivos.röda.RödaValue.BOOLEAN;
 import static org.kaivos.röda.RödaValue.INTEGER;
 
 import java.util.Arrays;
@@ -34,5 +35,14 @@ public final class SeqPopulator {
 			}
 		}, Arrays.asList(new Parameter("from", false, INTEGER), new Parameter("to", false, INTEGER)), false,
 				Arrays.asList(new Parameter("step", false, expressionInt("<seq populator>", 0, 1)))));
+		
+		S.setLocal("makeSeq", RödaNativeFunction.of("makeSeq", (typeargs, args, kwargs, scope, in, out) -> {
+			if (args.get(0).bool()) {
+				out.push(args.get(args.size()-1));
+				for (int i = args.size()-1; i >= 1; i--) {
+					in.pushBack(args.get(i));
+				}
+			}
+		}, Arrays.asList(new Parameter("cond", false, BOOLEAN), new Parameter("first_value", false), new Parameter("other_values", false)), true));
 	}
 }
